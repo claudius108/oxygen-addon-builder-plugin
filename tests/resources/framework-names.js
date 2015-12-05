f5cd811c10c3d63bdf7a34bb007591027e6af9f5
claudius108 <claudius.teodorescu@gmail.com>
2015-12-05 15:43:09 +0200

    Changed a path.

--- tests/resources/framework-names.js
+++ tests/resources/framework-names.js
@@ -2,308 +2,308 @@ frameworksNames = [ {
 	"title" : "frameworks",
 	"key" : "id7aea61af80ac4be6b2a08675393a3ae2",
 	"folder" : true,
-	"path" : "/home/claudius/oxygen/16/frameworks",
+	"path" : "/home/claudius/oxygen/current/frameworks",
 	"children" : [
 			{
 				"title" : "xinclude",
 				"key" : "idfe34b6f64b50476c8dd9afdfaf541d5c",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/xinclude",
+				"path" : "/home/claudius/oxygen/current/frameworks/xinclude",
 				"children" : []
 			},
 			{
 				"title" : "widgets",
 				"key" : "id82c57b23061e4093bb3fe77bbda01716",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/widgets",
+				"path" : "/home/claudius/oxygen/current/frameworks/widgets",
 				"children" : []
 			},
 			{
 				"title" : "relaxng",
 				"key" : "idc3ca6192aba84da081eac60a54bb48ce",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/relaxng",
+				"path" : "/home/claudius/oxygen/current/frameworks/relaxng",
 				"children" : []
 			},
 			{
 				"title" : "ncx",
 				"key" : "id4c6e12ee56a644bfa6c7a1df326fcfa3",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/ncx",
+				"path" : "/home/claudius/oxygen/current/frameworks/ncx",
 				"children" : []
 			},
 			{
 				"title" : "xslt",
 				"key" : "id33d0dc185672422e84ee4fb92464f292",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/xslt",
+				"path" : "/home/claudius/oxygen/current/frameworks/xslt",
 				"children" : []
 			},
 			{
 				"title" : "schematron",
 				"key" : "id9f97ab9ba0c74735b52eb9979113e25e",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/schematron",
+				"path" : "/home/claudius/oxygen/current/frameworks/schematron",
 				"children" : []
 			},
 			{
 				"title" : "test",
 				"key" : "id2c5ca272c75e4a03b52a969171d8660f",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/test",
+				"path" : "/home/claudius/oxygen/current/frameworks/test",
 				"children" : []
 			},
 			{
 				"title" : "xmlschema",
 				"key" : "id49bff8c46bca4dbba5c6be37ad66c70c",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/xmlschema",
+				"path" : "/home/claudius/oxygen/current/frameworks/xmlschema",
 				"children" : []
 			},
 			{
 				"title" : "xmldsig",
 				"key" : "id933ce52fc9b7491abdf33c80d28a68d9",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/xmldsig",
+				"path" : "/home/claudius/oxygen/current/frameworks/xmldsig",
 				"children" : []
 			},
 			{
 				"title" : "kml",
 				"key" : "idec2dbb20ae2345f59019c2e3af51e5ac",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/kml",
+				"path" : "/home/claudius/oxygen/current/frameworks/kml",
 				"children" : []
 			},
 			{
 				"title" : "css_support",
 				"key" : "id1f0b59dea5274903a8a37dce575cb4b7",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/css_support",
+				"path" : "/home/claudius/oxygen/current/frameworks/css_support",
 				"children" : []
 			},
 			{
 				"title" : "mathml",
 				"key" : "ide0e2461b9b3040ed9c5dbdb949040f4c",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/mathml",
+				"path" : "/home/claudius/oxygen/current/frameworks/mathml",
 				"children" : []
 			},
 			{
 				"title" : "xproc",
 				"key" : "id5b5a4f5e115f459882312e453b2b9598",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/xproc",
+				"path" : "/home/claudius/oxygen/current/frameworks/xproc",
 				"children" : []
 			},
 			{
 				"title" : "xforms",
 				"key" : "idca96ea8d099445d6aa5d6efcf61bd8bb",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/xforms",
+				"path" : "/home/claudius/oxygen/current/frameworks/xforms",
 				"children" : []
 			},
 			{
 				"title" : "daisy",
 				"key" : "ide7307c2486ae483bb24c50e9393ddee5",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/daisy",
+				"path" : "/home/claudius/oxygen/current/frameworks/daisy",
 				"children" : []
 			},
 			{
 				"title" : "stylesheet_documentation",
 				"key" : "id4c3f13f5d3fc44e2b46dffe684879d2b",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/stylesheet_documentation",
+				"path" : "/home/claudius/oxygen/current/frameworks/stylesheet_documentation",
 				"children" : []
 			},
 			{
 				"title" : "samples",
 				"key" : "id032240773d4843128c469af57b6bc6e9",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/samples",
+				"path" : "/home/claudius/oxygen/current/frameworks/samples",
 				"children" : []
 			},
 			{
 				"title" : "svg",
 				"key" : "id735673f865e14f5292c2283204841068",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/svg",
+				"path" : "/home/claudius/oxygen/current/frameworks/svg",
 				"children" : []
 			},
 			{
 				"title" : "ant",
 				"key" : "id5f1efcc410734e9193f83f18c1c72b8b",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/ant",
+				"path" : "/home/claudius/oxygen/current/frameworks/ant",
 				"children" : []
 			},
 			{
 				"title" : "opf",
 				"key" : "id5528fc6a9d3f4a008e66d135b17702e6",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/opf",
+				"path" : "/home/claudius/oxygen/current/frameworks/opf",
 				"children" : []
 			},
 			{
 				"title" : "xhtml",
 				"key" : "id8d88e9c38212408ca4fb3346725fc6d4",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/xhtml",
+				"path" : "/home/claudius/oxygen/current/frameworks/xhtml",
 				"children" : []
 			},
 			{
 				"title" : "ocf",
 				"key" : "idbc4fa4596d5442cf91b08e489eb3829e",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/ocf",
+				"path" : "/home/claudius/oxygen/current/frameworks/ocf",
 				"children" : []
 			},
 			{
 				"title" : "ead",
 				"key" : "id8910ae3ddf5f47a19252d63ea89cb808",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/ead",
+				"path" : "/home/claudius/oxygen/current/frameworks/ead",
 				"children" : []
 			},
 			{
 				"title" : "stratml",
 				"key" : "id20b48d0992eb4bfda11f20adf3abdceb",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/stratml",
+				"path" : "/home/claudius/oxygen/current/frameworks/stratml",
 				"children" : []
 			},
 			{
 				"title" : "fo",
 				"key" : "idb56508fbe5324465b028967b866023b5",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/fo",
+				"path" : "/home/claudius/oxygen/current/frameworks/fo",
 				"children" : []
 			},
 			{
 				"title" : "docbook",
 				"key" : "idc16fb8e759984fbf8dfbf48daaf2de91",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/docbook",
+				"path" : "/home/claudius/oxygen/current/frameworks/docbook",
 				"children" : []
 			},
 			{
 				"title" : "wsdl_documentation",
 				"key" : "ided63112469964c9cba591946e5f267d4",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/wsdl_documentation",
+				"path" : "/home/claudius/oxygen/current/frameworks/wsdl_documentation",
 				"children" : []
 			},
 			{
 				"title" : "xspec",
 				"key" : "idaa649be000a0422faa0ed985351d5730",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/xspec",
+				"path" : "/home/claudius/oxygen/current/frameworks/xspec",
 				"children" : []
 			},
 			{
 				"title" : "xhtml11",
 				"key" : "id2a63bf2fc7dd411cb61cc8f7b22b4b0d",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/xhtml11",
+				"path" : "/home/claudius/oxygen/current/frameworks/xhtml11",
 				"children" : []
 			},
 			{
 				"title" : "maven",
 				"key" : "id9c390d9339b5435486d98b9af88ea6bd",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/maven",
+				"path" : "/home/claudius/oxygen/current/frameworks/maven",
 				"children" : []
 			},
 			{
 				"title" : "tei",
 				"key" : "id5a877e21499d4a2fb1a46706109c78c7",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/tei",
+				"path" : "/home/claudius/oxygen/current/frameworks/tei",
 				"children" : []
 			},
 			{
 				"title" : "dita",
 				"key" : "id6fa712d18d55438fbe0fa0b8652b9141",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/dita",
+				"path" : "/home/claudius/oxygen/current/frameworks/dita",
 				"children" : []
 			},
 			{
 				"title" : "ooxml",
 				"key" : "id8d02e3b3d389422db037980f0ba33862",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/ooxml",
+				"path" : "/home/claudius/oxygen/current/frameworks/ooxml",
 				"children" : []
 			},
 			{
 				"title" : "oxygen",
 				"key" : "id52ecc64d9b384ec0a8fa35dc972a4ae6",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/oxygen",
+				"path" : "/home/claudius/oxygen/current/frameworks/oxygen",
 				"children" : []
 			},
 			{
 				"title" : "nvdl",
 				"key" : "idc451af86f7c24f95b2e296a2da1e0244",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/nvdl",
+				"path" : "/home/claudius/oxygen/current/frameworks/nvdl",
 				"children" : []
 			},
 			{
 				"title" : "xml",
 				"key" : "id63fcfed0520e418eb6b10aa6627b3917",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/xml",
+				"path" : "/home/claudius/oxygen/current/frameworks/xml",
 				"children" : []
 			},
 			{
 				"title" : "extensions",
 				"key" : "id9899f74426c841a8a5fa6f1358c6db6a",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/extensions",
+				"path" : "/home/claudius/oxygen/current/frameworks/extensions",
 				"children" : []
 			},
 			{
 				"title" : "schema_documentation",
 				"key" : "id933fc47b2944419da01e243d58a63370",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/schema_documentation",
+				"path" : "/home/claudius/oxygen/current/frameworks/schema_documentation",
 				"children" : []
 			}, {
 				"title" : "xlink",
 				"key" : "idf0cc336141cc4eb4aa976adda276f5ed",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/xlink",
+				"path" : "/home/claudius/oxygen/current/frameworks/xlink",
 				"children" : []
 			}, {
 				"title" : "xmlspec",
 				"key" : "iddc2d37d06a584c029d700e6bda5f8099",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/xmlspec",
+				"path" : "/home/claudius/oxygen/current/frameworks/xmlspec",
 				"children" : []
 			}, {
 				"title" : "dlri",
 				"key" : "id175aac6ec6264c71b2d8864b7b6e8815",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/dlri",
+				"path" : "/home/claudius/oxygen/current/frameworks/dlri",
 				"children" : []
 			}, {
 				"title" : "odf",
 				"key" : "ide75da5255e18472bb11374bc2f40c64f",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/odf",
+				"path" : "/home/claudius/oxygen/current/frameworks/odf",
 				"children" : []
 			}, {
 				"title" : "wsdl",
 				"key" : "id1ac6f2b120f54e889383d6fc604c2a74",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/wsdl",
+				"path" : "/home/claudius/oxygen/current/frameworks/wsdl",
 				"children" : []
 			}, {
 				"title" : "nerwsc",
 				"key" : "idc5b0f8a9dbb949a996d82287389af23c",
 				"folder" : true,
-				"path" : "/home/claudius/oxygen/16/frameworks/nerwsc",
+				"path" : "/home/claudius/oxygen/current/frameworks/nerwsc",
 				"children" : []
 			} ]
 } ];
